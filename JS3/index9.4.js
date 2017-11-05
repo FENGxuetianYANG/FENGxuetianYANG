c64b414eec6b52581fa98bec864d03f61b1553d0
FENGxuetianYANG <31703608+FENGxuetianYANG@users.noreply.github.com>
2017-11-05 21:57:03 +0800

    Add files via upload

---
+++ JS3/index9.4.js
@@ -0,0 +1,19 @@
+var a=100;
+var b=212;
+alert(a+b);
+alert(a*b);
+alert(b/b)
+var c=15;
+var d="20";
+alert(c-d+d);
+alert(c+d-d);
+alert(c*d-d);
+alert(d+d/d)
+
+var num1=43;
+var num2=40;
+var num3=num1+num2;
+var infor = "我们班有两个班组成，第一个班505有"+num1+"人，第二个班507有"+num2+"人，总共有"+num3+"人"
+console.log(infor)
+
+
